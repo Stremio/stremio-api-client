02554aff808c03570ce211db236c244ff94d2ae5
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2018-07-23 17:09:50 +0300

    simple ApiStore implemented

---
+++ apiStore.js
@@ -0,0 +1,42 @@
+var EventEmitter = require('events');
+var AddonCollection = require('stremio-addon-client').AddonCollection;
+var officialAddons = require('stremio-official-addons');
+var MemoryStorage = require('./lib/memoryStorage');
+var ApiClient = require('./apiClient');
+
+var ENDPOINT = 'https://api.strem.io';
+
+function ApiStore(options) {
+    var endpoint = options.endpoint || ENDPOINT;
+    var storage = options.storage || new MemoryStorage();
+    var client = new ApiClient(endpoint, storage.getJSON('authKey'));
+
+    this.events = new EventEmitter();
+    this.user = storage.getJSON('user');
+    this.addons = new AddonCollection(officialAddons);
+
+    this.request = function(method, params) {
+        var currentClient = client;
+        return new Promise(function(resolve, reject) {
+            currentClient.request(method, params)
+                .then(function(resp) {
+                    if (currentClient !== client) {
+                        reject(new Error('request interrupted'));
+                        return;
+                    }
+
+                    resolve(resp);
+                })
+                .catch(function(err) {
+                    if (currentClient !== client) {
+                        reject(new Error('request interrupted'));
+                        return;
+                    }
+
+                    reject(err);
+                });
+        });
+    };
+};
+
+module.exports = ApiStore;
